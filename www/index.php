5782db3aa46fc6bc215104108152e6354fd2ec88
Андрей Ширлинг <andrejsirling@MacBook-Air-Andrej.local>
2023-12-21 15:08:21 +0500

    head

--- www/index.php
+++ www/index.php
@@ -1,10 +1,12 @@
 <html>
+<head>
+    <link rel="manifest" href="/assets/manifest.webmanifest">
+    <script type="text/javascript" src="/assets/js/pwa/app.js"></script>
+</head>
 <body>
     <h1>Web Push talk @PWA Paris #2</h1>
     <button id="push-subscription-button">Push notifications !</button>
     <button id="send-push-button">Send a push notification</button>
-    <link rel="manifest" href="/assets/manifest.webmanifest">
-    <script type="text/javascript" src="/assets/js/pwa/app.js"></script>
     <script type="text/javascript" src="/serviceWorker.js"></script>
 </body>
 </html>
