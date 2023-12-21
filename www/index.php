37db52afaba5b2e1d818e18e87f3a1aa70110c3e
Андрей Ширлинг <andrejsirling@MacBook-Air-Andrej.local>
2023-12-21 14:44:35 +0500

    add traefik

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@
     <h1>Web Push talk @PWA Paris #2</h1>
     <button id="push-subscription-button">Push notifications !</button>
     <button id="send-push-button">Send a push notification</button>
-    <link rel="manifest" href="/assets/js/pwa//site.webmanifest">
+    <link rel="manifest" href="/assets/manifest.webmanifest">
     <script type="text/javascript" src="/assets/js/pwa/app.js"></script>
 </body>
 </html>
